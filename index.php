37f8f64cbc20933794da172b8393e083be68b9d5
unknown <cristianruiz1195@gmail.com>
2019-06-21 22:09:23 -0500

    This is a first commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Pagina ejemplo 3</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+	<header>
+		<div class="textos">
+			<h1 class="titulo">Cristian Ruiz</h1>
+			<h3 class="subtitulo">Subtitulo</h3>
+			<a href="#" class="boton">Suscribete</a>
+		</div>
+		<div class="sesgoabajo"></div>
+	</header>
+
+	<main>
+		<section class="acerca-de">
+			<div class="contenedor">
+				<h2 class="sobre-nosotros">Sobre nosotros</h2>
+				<h3 class="slogan">Nuestro Slogan</h3>
+				<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis aspernatur aliquam minima iste tenetur culpa.</p>
+				<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam accusantium aut ut error consequatur, molestiae alias deserunt suscipit, iusto consequuntur hic corporis quia. Mollitia ratione minima laborum sed ullam quae vel dolores necessitatibus eveniet repudiandae dolore aliquam inventore debitis, ea, excepturi beatae soluta facilis voluptatem aliquid expedita ad. Inventore architecto, numquam, sit nostrum praesentium ipsum ullam quae eveniet odit in, ad? Architecto similique, dolor possimus omnis nulla libero dolores, a mollitia sapiente labore doloribus nostrum voluptate!</p>
+
+				<a href="#" class="boton">Suscribete</a>
+			</div>
+		</section>
+
+		<section class="galeria">
+
+			<div class="sesgoarriba"></div>
+
+			<div class="imagenes none">
+				<img src="images/1.jpeg" alt="">
+			</div>
+			<div class="imagenes">
+				<img src="images/2.jpeg" alt="">
+			</div>
+			<div class="imagenes">
+				<img src="images/4.jpeg" alt="">
+				<div class="encima">
+					<h2>Cristian</h2>
+					<div></div>
+				</div>
+			</div>
+			<div class="imagenes">
+				<img src="images/3.jpeg" alt="">
+				
+			</div>
+			<div class="imagenes none">
+				<img src="images/5.jpeg" alt="">
+			</div>
+
+			<div class="sesgoabajo"></div>
+
+		</section>
+
+		<section class="miembros">
+			<div class="contenedor">
+				<h2 class="sobre-nosotros">Nuestro equipo</h2>
+				<h3 class="slogan">Conoce a nuestro equipo de trabajo</h3>
+				<div class="cards">
+					<div class="card">
+						<img src="images/logo_ittg.png" alt="">
+						<h4>CRistian Ruiz</h4>
+						<p>Lorem ipsum dolor sit.</p>
+					</div>
+					<div class="card">
+						<img src="images/logo_ittg.png" alt="">
+						<h4>CRistian Ruiz</h4>
+						<p>Lorem ipsum dolor sit.</p>
+					</div>
+					<div class="card">
+						<img src="images/logo_ittg.png" alt="">
+						<h4>CRistian Ruiz</h4>
+						<p>Lorem ipsum dolor sit.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="fondo">
+			<div class="sesgoarriba"></div>
+			<div class="contenedor">
+				<h2 class="titulo-patrocinadores">Nuestros Clientes</h2>
+				<h3 class="subtitulo-patrocinadores">Conoce a algunos de nuestros patrocinadores</h3>
+				<div class="clientes">
+					<div class="cliente">
+						<img src="images/logo_ittg.png" alt="">
+					</div>
+					<div class="cliente">
+						<img src="images/logo_ittg.png" alt="">
+					</div>
+					<div class="cliente">
+						<img src="images/logo_ittg.png" alt="">
+					</div>
+				</div>
+				<h3 class="subtitulo-patrocinadores especial">y muchos más clientes</h3>
+			</div>
+
+			<div class="sesgoabajo-unico"></div>
+		</section>
+	</main>
+
+	<footer>
+		<div class="contenedor">
+			<h2 class="titulo-footer">Contactanos</h2>
+			<h3 class="subtitulo-footer">Los apreciamos mucho</h3>
+			<div class="footer">
+				<form action="">
+					<input type="text" name="" id="" placeholder="Nombre">
+					<input type="email" name="" id="" placeholder="Email">
+					<textarea name="" id="" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
+					<input type="submit" value="Enviar">
+				</form>
+			</div>
+		</div>
+	</footer>
+	
+</body>
+</html>
